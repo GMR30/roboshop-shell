4251903f863d691c96ad6993b1c3536c5682d133
raju2 <raj.sresta@gmail.com>
2024-05-29 20:31:25 +0530

    In frontend,we are installing nginx

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,15 @@
 echo -e "\e[31m***Installing Nginx\e[0m"
-dnf install nginx -y    >/tmp/roboshop.log
+dnf install nginx -y    2>>/tmp/roboshop.log
 echo -e "\e[32m***Removing old content\e[0m"
-rm -rf /usr/share/nginx/html/*  >/tmp/roboshop.log
+rm -rf /usr/share/nginx/html/*  2>>/tmp/roboshop.log
 echo -e "\e[33m***Downloading frontend zip file\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  >/tmp/roboshop.log 2>/tmp/error.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  2>>/tmp/roboshop.log
 echo -e "\e[34m***Extracting the frontend unzip\e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip   >/tmp/roboshop.log
+unzip /tmp/frontend.zip   2>>/tmp/roboshop.log
 #echo -e "\e[36m***Hello RED Color\e[0m"
 #vim /etc/nginx/default.d/roboshop.conf
 #we need to copy config file
 echo -e "\e[35m***Enable and Restart the Nginx service\e[0m"
-systemctl enable nginx    >/tmp/roboshop.log
-systemctl restart nginx   >/tmp/roboshop.log
+systemctl enable nginx    2>>/tmp/roboshop.log
+systemctl restart nginx   2>>/tmp/roboshop.log

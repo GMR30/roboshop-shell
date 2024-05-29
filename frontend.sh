f2f262c0276d5ea154d99074c34ec89eb8849ada
raju2 <raj.sresta@gmail.com>
2024-05-29 20:22:07 +0530

    In frontend,we are installing nginx

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,7 @@
 echo -e "\e[31m***Installing Nginx\e[0m"
 dnf install nginx -y    >/tmp/roboshop.log
 echo -e "\e[32m***Removing old content\e[0m"
-rm -rf /usr/share/nginx/html/*  >/tmp/roboshop.log  >/tmp/roboshop.log
+rm -rf /usr/share/nginx/html/*  >/tmp/roboshop.log
 echo -e "\e[33m***Downloading frontend zip file\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  >/tmp/roboshop.log
 echo -e "\e[34m***Extracting the frontend unzip\e[0m"

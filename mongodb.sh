34cd7f68fd375332643bfc6cccf39c28ec1e228a
raju2 <raj.sresta@gmail.com>
2024-06-02 19:13:11 +0530

    In frontend,we are installing nginx

--- mongodb.sh
+++ mongodb.sh
@@ -2,7 +2,8 @@ echo -e "\e[31m***Copying  Mongodb repo file\e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 echo -e "\e[32m***Installing Mongodb\e[0m"
 dnf install mongodb-org -y  &>>/tmp/roboshop.log
-#echo -e "\e[33m***Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf\e[0m"
+echo -e "\e[33m***Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf\e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo -e "\e[34m***Enable and Restart Mongodb service\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod  &>>/tmp/roboshop.log
